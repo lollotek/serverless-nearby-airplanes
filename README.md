19781b642a16b16a79bbb83d8dfb1b7a1de1e3ed
Lorenzo Boasso <lorenzo.boasso@gmail.com>
2021-10-03 11:53:35 +0200

    feat: add airplane tracker

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # serverless-nearby-airplanes
 serverless for netlify that returns nearby airplanes using opensky data
+
+# use:
+send into body the information for long lat and range:
+```
+{
+  "latitude": "45.577441",
+  "longitude": "9.0938987",
+  "range": "2"
+}
+```
+
+# sample guide:
+https://docs.netlify.com/functions/build-with-javascript/#unbundled-javascript-function-deploys
+
+# debug:
+instruction at: https://cli.netlify.com/vscode/#debugging
--- functions/airplanes.ts
+++ functions/airplanes.ts
@@ -1,18 +1,45 @@
-const querystring = require("querystring");
+import fetch from 'node-fetch'
+import { Handler } from "@netlify/functions";
+import { argMin, validCoordinates, jsonifyPlaneState, openskyLatLonString } from './tools/utils';
 
-exports.handler = async (event, context) => {
+const API_ENDPOINT = 'https://opensky-network.org/api'
+
+const handler: Handler = async (event, _) => {
+  
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
+  
+  const {latitude, longitude, range} = JSON.parse(event.body);
+  const validCoord = validCoordinates(latitude, longitude);
+  if (!validCoord.valid) {
+    return { statusCode: 400, body: validCoord.body };
+  }
+  const lat = validCoord.body.lat
+  const lon = validCoord.body.lon
 
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body);
-  const name = params.name || "World";
+  console.log(`${API_ENDPOINT}/states/all?${openskyLatLonString(lat, lon, range)}`)
 
-  return {
-    statusCode: 200,
-    body: `Hello, ${name}`,
-  };
-};
+  return fetch(`${API_ENDPOINT}/states/all?${openskyLatLonString(lat, lon, range)}`)
+    .then(response => response.json())
+    .then((data: {states:Array<Array<string | number | boolean>>}) => {
+      let states = data.states;
+      const dist = (state) => Math.sqrt((state[5]-lon)**2 + (state[6]-lat)**2);
+              
+      // Find nearest plane in array
+      const nearest = states[argMin(states, dist)];
+      // Convert State array to JSON
+      const body = JSON.stringify(jsonifyPlaneState(nearest));
+      // res.status(200).send(body);
+      return  { statusCode: 200, body: body};
+    })
+    .catch(err => {
+        return  {
+          statusCode: err.statusCode || 500,
+          body: JSON.stringify({error: err.message})
+        };
+    });
+}
+
+export { handler };
